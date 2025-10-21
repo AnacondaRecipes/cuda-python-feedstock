9ad8c813c61300c88bb0e9616745e657fb5c63b5
Onur Bingol <obingol@anaconda.com>
2025-10-21 07:34:03 -0700

    cleanup temporary .c and .cpp files after pip install

--- recipe/build_output.bat
+++ recipe/build_output.bat
@@ -1,3 +1,8 @@
+:: Build subpackage
 set DIR_NAME=%PKG_NAME:-=_%
 cd %DIR_NAME%
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
+
+:: Clean up Cython temporary files from site-packages directory
+del /s /q %SP_DIR%\*.c
+del /s /q %SP_DIR%\*.cpp
