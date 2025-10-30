1b4fa4fd2e10f60d44e64238be19aeaaf3d07c1f
Onur Bingol <61478203+onurbingol@users.noreply.github.com>
2025-10-30 15:25:24 -0700

    Release v12.8.0
    cuda-python 12.8.0
    
    **Destination channel:** defaults
    
    ### Links
    
    - [PKG-9811](https://anaconda.atlassian.net/browse/PKG-9811) 
    - [Upstream
    repository](https://github.com/NVIDIA/cuda-python/tree/v12.8.0)
    - [Upstream changelog/diff]()
    - Relevant dependency PRs:
    - pyclibrary v0.3.0:
    https://github.com/AnacondaRecipes/pyclibrary-feedstock/pull/1
    - CUDA 12.8.1: https://github.com/AnacondaRecipes/cuda-feedstock/pull/2
    
    ### Explanation of changes:
    
    - Initial release of `cuda-python`, `cuda-bindings` and `cuda-core` on
    conda main channel
    - Clean up variables in `conda_build_config.yaml` and `meta.yaml`
    - Fix depreciation warnings in `test_cimport,pyx`
    - Add `--no-build-isolation` to build scripts
    - Update recipe file to make it more future proof in handing additional
    subpackages coming with the newer versions of `cuda-python`. There are a
    good number of changes between `cuda-python` v12.8.0 and v13.0.2 recipes
    [ref](https://github.com/conda-forge/cuda-python-feedstock/commits/main/?since=2025-02-02&until=2025-10-20).
    - Updated `about` sections for individual subpackages
    
    ### PR review suggestions:
    
    - Dependencies under `host`, `build` and `run` sections
    - Usage of `pin_compatible`, `pin_subpackage`
    - Removal of cython-generated source files, e.g. `*.c` and/or `*.cpp`
    - `{{ compiler('cuda') }}` vs `__cuda` usage
    - Any additional comments and suggestions are always welcome!
    
    ### Notes:
    
    - ~`linux-aarch64` builds are failing due to a PBP memory issue~
    - The upstream repository has tests for the included subpackages, but
    the progenitor recipe from conda-forge won't run them.
    - Linter error and warnings were investigated, no issues to leave them
    as they are.
    
    [PKG-9811]:
    https://anaconda.atlassian.net/browse/PKG-9811?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- recipe/build_output.sh
+++ recipe/build_output.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Build subpackage
 DIR_NAME="$(echo $PKG_NAME | tr '-' '_')"
 cd $DIR_NAME
-$PYTHON -m pip install . --no-deps -vv
+$PYTHON -m pip install . --no-deps --no-build-isolation -vv
+
+# Clean up Cython temporary files from site-packages directory
+find $SP_DIR -name "*.c" -type f -delete
+find $SP_DIR -name "*.cpp" -type f -delete
