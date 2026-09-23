18eb9fab8a422842afd69db9e05b5453165cc10b
Onur R. Bingol <obingol@anaconda.com>
2026-09-22 21:05:00 -0700

    update build scripts

--- recipe/build_output.bat
+++ recipe/build_output.bat
@@ -4,11 +4,7 @@ set "LIB=%PREFIX%\\%SUBDIR%\\lib\\%TARGET_NAME%;%LIB%"
 
 set DIR_NAME=%PKG_NAME:-=_%
 cd %DIR_NAME%
-IF "%DIR_NAME%"=="cuda_python" (
-  REM The upstream setup.py calls get_version() without dist_name,
-  REM so only the generic SETUPTOOLS_SCM_PRETEND_VERSION is honored.
-  SET "SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%"
-)
+
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
